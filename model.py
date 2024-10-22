760ceade47d5b04b5215c55db9f64aeedea24c3d
cveluru <chaithanyakranthi@gmail.com>
2024-10-22 16:15:25 +0530

    Update model.py

--- model.py
+++ model.py
@@ -5,44 +5,6 @@ import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 import pickle
-import nltk
-nltk.download('stopwords')
-nltk.download('wordnet')
-from nltk.corpus import stopwords
-from nltk.stem import WordNetLemmatizer
-
-wordLemmatizer = WordNetLemmatizer()
-
-# Get the finalized artifacts i.e. TFIDF vectorizer, the sentiment prediction model
-# and the recommendation ratings
-tfidf_transformer = pickle.load(open('pickles/tfidf_vectorizer.pkl','rb'))
-sentiment_model = pickle.load(open('pickles/lr.pkl', 'rb'))
-user_ratings = pickle.load(open('pickles/user_final_rating.pkl', 'rb'))
-
-# Get the review data for all users and products
-df_reviews = pd.read_csv('sample30.csv')
-
-# Impute missing manufacturer 
-df_reviews.loc[df_reviews[pd.isnull(df_reviews.manufacturer)].index,'manufacturer'] = 'Summit Entertainment'
-
-# Get rated and valid user names
-rated_users = user_ratings[(user_ratings != 0).any(axis=1)].index.to_list()
-all_users = user_ratings.index.to_list()
-
-
-def get_lemmatized(in_text):
-    ''' This function handles the lemmatized of the given text
-      - Remove any unwanted characters including punctuation
-      - Convert the text into lower case
-      - Tokenize the text into words
-      - Get the lemmas for the tokens after ignoring stopwords
-      - Finally get the result text by joining the lemmas
-    '''
-    text = re.sub(r"[^a-zA-Z0-9\s,']",'',in_text)
-    text = text.lower()
-    text = text.split()
-    text = [wordLemmatizer.lemmatize(word) for word in text if word not in set(stopwords.words('english'))]
-    return " ".join(text)
 
 
 def valid_user(user):
@@ -77,7 +39,7 @@ def get_recommended_products(user):
         top20_products = pd.DataFrame(user_ratings.loc[user].sort_values(ascending=False)[0:20])
 
         # Get all reviews for the top 20 products recommended
-        df_reviews_20 = df_reviews.loc[(df_reviews.name.isin(top20_products.index.tolist())),['name','brand','manufacturer','reviews_text']]
+        df_reviews_20 = df_reviews.loc[(df_reviews.name.isin(top20_products.index.tolist())),['name','brand','manufacturer','lemma_review']]
 
         # Filter top 5 of the 20 recommended using sentiment prediction
         df_top5 = get_top5_recommended_products(df_reviews_20)
@@ -95,9 +57,6 @@ def get_top5_recommended_products(df):
       - it uses the sentiment model to predict sentiment based on the lemmatized review texts
       - then, proposes the top 5 products using with highest % positive sentiments
     '''
-    # get lemmatized text
-    df['lemma_review'] = df['reviews_text'].apply(lambda x: get_lemmatized(x))
-
     # predict the sentiment for the selected products by first extracting the features from the lemmatized review
     df['pred_sent'] = sentiment_model.predict(tfidf_transformer.transform(df['lemma_review']))
 
@@ -114,3 +73,21 @@ def get_top5_recommended_products(df):
     print(df3_summary.head())
 
     return df3_summary.head()
+
+
+# Get the finalized artifacts i.e. TFIDF vectorizer, the sentiment prediction model
+# and the recommendation ratings
+tfidf_transformer = pickle.load(open('pickles/tfidf_vectorizer.pkl','rb'))
+sentiment_model = pickle.load(open('pickles/lr.pkl', 'rb'))
+user_ratings = pickle.load(open('pickles/user_final_rating.pkl', 'rb'))
+
+# Get rated and valid user names
+rated_users = user_ratings[(user_ratings != 0).any(axis=1)].index.to_list()
+all_users = user_ratings.index.to_list()
+
+# Since we are working with a static list of reviews 'sample30.csv', 
+# will optimize the prediction performance for the app by already using
+# the cleaned sample data (already handled data and text cleaning)
+df_reviews = pickle.load(open('pickles/cleaned_reviews.pkl', 'rb'))
+
+
